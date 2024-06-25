445d0f273a426b1955f5760d445ac7ef3d1a6e05
phj <aaagowns@gmail.com>
2024-06-25 22:14:50 +0900

    #6 Header 스타일링

---
+++ client/src/components/common/Header.tsx
@@ -0,0 +1,86 @@
+import styled from "styled-components";
+import { FaRegUser } from "react-icons/fa";
+import { FaBagShopping } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <HeaderStyle>
+      <div className="header">
+        <h1 className="logo">Soo Closet 🍃</h1>
+        <nav>
+          <Link to="/login">
+            <FaRegUser />
+          </Link>
+          <Link to="/cart">
+            <FaBagShopping />
+          </Link>
+        </nav>
+      </div>
+      <ul className="category">
+        <li>
+          <Link to="/">New</Link>
+        </li>
+        <li>
+          <Link to="/">Outer</Link>
+        </li>
+        <li>
+          <Link to="/">Top</Link>
+        </li>
+        <li>
+          <Link to="/">Bottom</Link>
+        </li>
+        <li>
+          <Link to="/">Acc</Link>
+        </li>
+      </ul>
+    </HeaderStyle>
+  );
+}
+const HeaderStyle = styled.header`
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.secondary};
+  position: relative;
+
+  .header {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding: 20px 80px;
+
+    .logo {
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+      font-size: ${({ theme }) => theme.heading.large.fontSize};
+    }
+
+    nav {
+      display: flex;
+      gap: 20px;
+      svg {
+        font-size: ${({ theme }) => theme.heading.medium.fontSize};
+      }
+    }
+  }
+
+  .category {
+    display: flex;
+    gap: 20px;
+    justify-content: center;
+    background-color: ${({ theme }) => theme.colors.secondary};
+    padding: 10px 0 0 0;
+    li {
+      list-style: none;
+      a {
+        color: ${({ theme }) => theme.colors.primary};
+        text-decoration: none;
+        font-size: ${({ theme }) => theme.heading.small.fontSize};
+        font-weight: bold;
+      }
+    }
+  }
+`;
+
+export default Header;
