0f3bbc78a9ec6981e5e75bb51c01092ff0f5f490
phj <aaagowns@gmail.com>
2024-06-26 13:09:28 +0900

    #6 footer 추가

---
+++ client/src/components/common/Footer.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+function Footer() {
+  return (
+    <FooterStyle>
+      <h1>Soo Closet 🍃</h1>
+      <hr />
+      <div className="copyright">
+        <p>copyright(c), 2024, Soo Closet</p>
+      </div>
+    </FooterStyle>
+  );
+}
+const FooterStyle = styled.div``;
+
+export default Footer;
