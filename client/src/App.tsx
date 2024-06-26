b93f4279e186fada3f538d5dfdbb78fd88156bd0
phj <aaagowns@gmail.com>
2024-06-26 16:20:47 +0900

    #6 회원가입/로그인 라우터 추가
    - react-hook-form 설치

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import Layout from "./components/Layout/Layout";
-import { GlobalStyle } from "./style/global";
-import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./style/theme";
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Home from "./pages/Home"
+import Layout from "./components/Layout/Layout"
+import { GlobalStyle } from "./style/global"
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./style/theme"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 
 function App() {
   const routerList = [
@@ -11,16 +13,24 @@ function App() {
       path: "/",
       element: <Home />,
     },
-  ];
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/signup",
+      element: <Signup />,
+    },
+  ]
 
   const newRouterList = routerList.map((route) => {
     return {
       ...route,
       element: <Layout>{route.element}</Layout>,
-    };
-  });
+    }
+  })
 
-  const router = createBrowserRouter(newRouterList);
+  const router = createBrowserRouter(newRouterList)
 
   return (
     <>
@@ -29,7 +39,7 @@ function App() {
         <RouterProvider router={router} />
       </ThemeProvider>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
