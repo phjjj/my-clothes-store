d6c7d070c5ca1ff0ce9928f4fa924621a34f83d2
phj <aaagowns@gmail.com>
2024-06-26 23:09:52 +0900

    #4 Fix : cors 추가, 라우터 주소 수정

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,38 @@
 // app.js (또는 index.js)
-import express from "express";
-import { connectDB } from "./db/connectDB.js";
-import UserRouter from "./routes/UserRouter.js";
-import ProductRouter from "./routes/ProductRouter.js";
-import OrderRouter from "./routes/OrderRouter.js";
-import CartRouter from "./routes/CartRouter.js";
-import { postRefreshToken } from "./Controllers/UserController.js";
+import express from "express"
+import { connectDB } from "./db/connectDB.js"
+import UserRouter from "./routes/UserRouter.js"
+import ProductRouter from "./routes/ProductRouter.js"
+import OrderRouter from "./routes/OrderRouter.js"
+import CartRouter from "./routes/CartRouter.js"
+import { postRefreshToken } from "./Controllers/UserController.js"
+import cors from "cors"
 
-const app = express();
-const PORT = 7777;
+const app = express()
+const PORT = 7777
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // 이 부분을 추가해줘야 req.body에 접근 가능
+app.use(express.json())
+app.use(express.urlencoded({ extended: true })) // 이 부분을 추가해줘야 req.body에 접근 가능
+// cors 설정
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    exposedHeaders: ["Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+)
 
-connectDB();
+connectDB()
 
 // 라우터 설정
-app.use("/user", UserRouter);
-app.use("/products", ProductRouter);
-app.use("/orders", OrderRouter);
-app.use("/cart", CartRouter);
+app.use("/users", UserRouter)
+app.use("/products", ProductRouter)
+app.use("/orders", OrderRouter)
+app.use("/cart", CartRouter)
 
-app.use("/refresh-token", postRefreshToken);
+app.use("/refresh-token", postRefreshToken)
 
 app.listen(PORT, () => {
-  console.log(`🍿Server is running on port http://localhost:${PORT}`);
-});
+  console.log(`🍿Server is running on port http://localhost:${PORT}`)
+})
