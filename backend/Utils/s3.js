967363180fa9aebb99b81db53e23a8d94558e4da
phj <aaagowns@gmail.com>
2024-06-27 20:53:36 +0900

    #4 Feat : 이미지 리사이징 기능 추가
    - shapr 라이브러리 설치

--- backend/Utils/s3.js
+++ backend/Utils/s3.js
@@ -1,30 +1,33 @@
-import AWS from "aws-sdk";
-import { v4 as uuidv4 } from "uuid";
-import dotenv from "dotenv";
-dotenv.config();
+import AWS from "aws-sdk"
+import { v4 as uuidv4 } from "uuid"
+import dotenv from "dotenv"
+dotenv.config()
+import { resizeImage } from "./sharp.js"
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-});
+})
 
 export const uploadToS3 = async (file, fileName) => {
+  // 이미지 리사이징
+  const resizedImage = await resizeImage(file)
   try {
     const params = {
       Bucket: process.env.AWS_BUCKET_NAME,
       Key: fileName, // 파일 이름
-      Body: file.buffer, // 파일의 버퍼를 직접 사용
+      Body: resizedImage, // 파일 데이터
       ContentType: file.mimetype,
-    };
+    }
 
-    const data = await s3.upload(params).promise();
+    const data = await s3.upload(params).promise()
 
-    return data.Location;
+    return data.Location
   } catch (error) {
-    console.error(error);
-    throw error;
+    console.error(error)
+    throw error
   }
-};
+}
 
 // 특정 접두사를 가진 객체를 삭제하는 함수
 export const deleteObjectsByPrefix = async (prefix) => {
@@ -34,22 +37,22 @@ export const deleteObjectsByPrefix = async (prefix) => {
         Bucket: process.env.AWS_BUCKET_NAME,
         Prefix: prefix,
       })
-      .promise();
+      .promise()
 
     if (!data.Contents.length) {
-      return;
+      return
     }
 
-    const objects = data.Contents.map((object) => ({ Key: object.Key }));
+    const objects = data.Contents.map((object) => ({ Key: object.Key }))
 
     await s3
       .deleteObjects({
         Bucket: process.env.AWS_BUCKET_NAME,
         Delete: { Objects: objects },
       })
-      .promise();
+      .promise()
   } catch (error) {
-    console.error(error);
-    throw error;
+    console.error(error)
+    throw error
   }
-};
+}
