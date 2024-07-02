ff19e915bfe5399515a017719c764070fc049276
phj <aaagowns@gmail.com>
2024-07-03 01:44:45 +0900

    #4 Chore : 들여쓰기, 세미콜론

--- backend/Services/UserService.js
+++ backend/Services/UserService.js
@@ -1,64 +1,60 @@
-import bcrypt from "bcrypt";
-import UserModel from "../models/UserModel.js";
-import {
-  createAccessToken,
-  createRefreshToken,
-  verifyRefreshToken,
-} from "../utils/jwt.js";
+import bcrypt from "bcrypt"
+import UserModel from "../models/UserModel.js"
+import { createAccessToken, createRefreshToken, verifyRefreshToken } from "../utils/jwt.js"
 
 const registerUser = async (user) => {
-  const hashedPassword = await bcrypt.hash(user.password, 10);
-  user.password = hashedPassword;
+  const hashedPassword = await bcrypt.hash(user.password, 10)
+  user.password = hashedPassword
 
-  return await UserModel.createUser(user);
-};
+  return await UserModel.createUser(user)
+}
 
 const loginUser = async (email, password) => {
-  const user = await UserModel.getUserByEmail(email);
+  const user = await UserModel.getUserByEmail(email)
 
-  if (!user) throw new Error("User not found");
+  if (!user) throw new Error("User not found")
 
-  const isPasswordValid = await bcrypt.compare(password, user.password);
+  const isPasswordValid = await bcrypt.compare(password, user.password)
 
-  if (!isPasswordValid) throw new Error("Invalid password");
+  if (!isPasswordValid) throw new Error("Invalid password")
 
-  const accessToken = createAccessToken(user.id);
-  const refreshToken = createRefreshToken(user.id);
+  const accessToken = createAccessToken(user.id)
+  const refreshToken = createRefreshToken(user.id)
 
-  await UserModel.saveRefreshToken(user.id, refreshToken);
+  await UserModel.saveRefreshToken(user.id, refreshToken)
 
-  return { accessToken, refreshToken };
-};
+  return { accessToken, refreshToken }
+}
 
 const checkRefreshToken = async (oldRefreshToken) => {
-  let userId;
+  let userId
   try {
-    const decoded = jwt.verifyRefreshToken(oldRefreshToken);
-    userId = decoded.userId;
+    const decoded = verifyRefreshToken(oldRefreshToken)
+    userId = decoded.uid
   } catch (error) {
-    throw new Error("Invalid refresh token");
+    throw new Error("Invalid refresh token")
   }
 
   // DB에 저장된 refresh token과 비교
-  const savedRefreshToken = await UserModel.getRefreshToken(userId);
+  const savedRefreshToken = await UserModel.getRefreshToken(userId)
 
   // 저장된 refresh token과 요청한 refresh token이 다르면 에러 발생
   if (oldRefreshToken !== savedRefreshToken) {
-    throw new Error("Invalid refresh token");
+    throw new Error("Invalid refresh token")
   }
 
-  const newAccessToken = jwt.createAccessToken(userId);
-  const newRefreshToken = jwt.createRefreshToken(userId);
+  const newAccessToken = createAccessToken(userId)
+  const newRefreshToken = createRefreshToken(userId)
 
   // 새로운 refresh token으로 갱신
-  await UserModel.saveRefreshToken(userId, newRefreshToken);
+  await UserModel.saveRefreshToken(userId, newRefreshToken)
 
-  return { newAccessToken, newRefreshToken };
-};
+  return { newAccessToken, newRefreshToken }
+}
 
 // 유저 정보 조회
 const getUser = async (userId) => {
-  return await UserModel.getUserById(userId);
-};
+  return await UserModel.getUserById(userId)
+}
 
-export default { registerUser, loginUser, checkRefreshToken, getUser };
+export default { registerUser, loginUser, checkRefreshToken, getUser }
--- backend/routes/CartRouter.js
+++ backend/routes/CartRouter.js
@@ -1,15 +1,11 @@
-import express from "express";
-import {
-  postCartItem,
-  getCartItems,
-  deleteCartItem,
-} from "../controllers/CartController.js";
-import authenticateJWT from "../middleware/authMiddleware.js";
+import express from "express"
+import { postCartItem, getCartItems, deleteCartItem } from "../controllers/CartController.js"
+import authenticateJWT from "../middleware/authMiddleware.js"
 
-const CartRouter = express.Router();
+const CartRouter = express.Router()
 
-CartRouter.post("/", authenticateJWT, postCartItem);
-CartRouter.get("/", authenticateJWT, getCartItems);
-CartRouter.delete("/:id", authenticateJWT, deleteCartItem);
+CartRouter.post("/", authenticateJWT, postCartItem)
+CartRouter.get("/", authenticateJWT, getCartItems)
+CartRouter.delete("/:id", authenticateJWT, deleteCartItem)
 
-export default CartRouter;
+export default CartRouter
